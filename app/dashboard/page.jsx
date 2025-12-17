345ae94c928f53df1bd13dd86a1c247adb78f1e6
Emmanuel9037894958 <emmanuelamamchukwu@gmail.com>
2025-12-17 13:21:19 +0100

    checking

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -66,7 +66,7 @@
 export default function DashboardPage() {
   return (
     <div style={{ color: "white", padding: 40 }}>
-      <h1>Dashboard is rendering</h1>
+      <h1 className="py-44">Dashboard is rendering</h1>
     </div>
   );
 }
